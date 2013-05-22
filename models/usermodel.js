b69b292fb0a8bc79184ee5ce4f47988ef054ee89
steve kane <skane@4-south.com>
2013-05-21 20:01:04 -0500

    massively restructured backend to be more modular

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,3 +1,39 @@
-var brcrypt = require('bcrypt')
- ,  mongoose = require('mongoose')
- ,  SALT_WORK_FACTOR = 10;
+var mongoose = require('mongoose')
+  , ModelClass = require('./model');
+
+/*
+Models are declared in the semantics of the ORM of choice
+I.E. If a user is using a mongoose model, then they will build
+their models in mongoose's semantics and then create an instance 
+of the corrosponding Model object that requires the mongoose model
+in its constructor
+*/
+
+/*
+create an instance of a mongoose schema that includes encryption
+provided by the bcrypt module
+*/
+var UserSchema = new mongoose.Schema({
+  
+  username: { type: String,
+              required: true,
+              index: { unique: true } },
+
+  password: { type: String,
+              required: true },
+
+  email:    { type: String,
+              required: true,
+              unique: true }
+});
+
+/*
+this is the mongoose model we will require as a dependency of our
+mongoose-based model implementation in the constructor
+*/
+var User = mongoose.model("User", UserSchema);
+
+console.log(UserModel);
+var UserModel = new ModelClass(User);
+
+module.exports = UserModel;
--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,85 @@
+var async = require('async')
+  , userController = require('./../controllers/usercontroller')
+  , send200 = require('./utils').send200
+  , send400 = require('./utils').send400;
+ 
+
 /*
 All methods will return valid 200 HTTP responses if successful
 and otherwise will return 400 HTTP responses with a json body 
-"errorMessage": "some message" which may be used to print to the
+"message": "some message" which may be used to print to the
 user on the front-end
 */
 
-//create a new user and log them in
+//create a new user after checking that this username/email dont exist already
 exports.createUser = function ( req, res ) {
-  //validate username and password
-  //hash the password and store new user in db
-  res.send('createuser');
+  var email = req.body.email
+    , username = req.body.username
+    , password = req.body.password;
+
+  //check if user by this username or email already exists
+  aysnc.parallel([
+    function (callback) {
+      userController.findUserByQuery({username: username}, function (err, user) {
+        callback(err, user);
+      });
+    },
+    function (callback) {
+      userController.findUserByQuery({email: email}, function (err, user) {
+        callback(err, user);
+      });
+    },
+  ], function (err, results) {
+    if (err ) { send400(res, err); }
+    
+    /*
+    async returns an array of possible results, check them for content
+    if either has returned a user, it means a user matching the query
+    was already in the db so prevent another from being created
+    */
+    if (results[0]) { send400('user by that username already exists'); }
+    if (results[1]) { send400('user with that email already exists'); }
+
+    userController.createUser(req.body, function (err, user) {
+      if (err) { send400(res, err); }
+    
+      console.log(user, 'created');    
+      //delete email and then pass the username/pw to generate a token
+      delete req.body.email;
+      next();
+    });  
+  });
 };
 
 exports.updateUser = function ( req, res ) {
-  //validate username and password
-  //update the user in the db
-  res.send('updateuser');
+  var updateHash = { 
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email 
+  };
+  userController.findUserById(req.params.id, updateHash, function(err, result) {
+    if (err) { send400(res, err); }
+    var response = {};
+
+    response['user'] = result;
+    send200(res, response);  
+  });
 };
 
 exports.deleteUser = function ( req, res ) {
-  //delete this user from the db
-  //delete models that are related to this user
-  res.send('deleteuser');
+  userController.deleteUser(req.params.id, function (err, result) {
+    if (err) { send400(res, err); }
+
+    send200(res, {});
+  });
 };
 
+//only called if login successful, sends id and username
 exports.postLogin = function ( req, res ) {
-  //return username and user info (not password!)
-  //return a token/session cookie
-  res.send('postLogin');
+  send200({id: req.user.id, username: req.user.username});
 };
 
+//called after successful logout
 exports.getLogout = function ( req, res ) {
-  //destroys session cookie/token associated w/ this user
-  //return logout confirmation
-  res.send('getLogout')
+  send200(res, {});
 };
